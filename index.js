75a1100ebefd70f28f4af1992fac79fd5d8f15cd
zeejay21 <>
2021-06-08 16:09:02 +0000

    Exercise complete

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ let arr = ['B', 'n', 'n', 5];
 
 //a) Use the split method on the string to identify the purpose of the parameter inside the ().
 
-
+console.log(str.split());
+console.log(str.split("e"));
+console.log(str.split(" "));
+console.log(str.split(""));
+console.log("====\n")
+//It splits eleements based on  what we put inside the ()
 
 //b) Use the join method on the array to identify the purpose of the parameter inside the ().
+console.log(arr);
+console.log(arr.join());
+console.log(arr.join("a"));
+console.log(arr.join(" "));
+console.log(arr.join(""));
+console.log("====\n")
 
-
+//The purpose of join is
 
 //c) Do split or join change the original string/array?
 
+console.log(str.split(',').sort().join(','));
 
+console.log(str);
+console.log("====\n")
+
+//No it doesnt change the orignal array
 
 //d) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+
+let newArray=[];
+console.log(cargoHold);
+newArray = cargoHold.split(",");
+console.log(newArray);
+console.log("Alphabetized");
+newArray.sort();
+console.log("New string: ");
+newArray.join(" ");
